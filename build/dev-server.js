78d82ada20235d3a8221c62f85d490f461f7dd39
Ivo Georgiev <ivo@linvo.org>
2018-02-14 02:28:32 +0200

    basic stuff for building, depsblob

---
+++ build/dev-server.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var pkg = require('../package')
+
+// Serve development assets - compile jade/stylus on demand
+var express = require('express')
+
+var fs = require('fs')
+var url = require('url')
+var depsBlob = require('./browserify-client-deps')
+var router = express.Router()
+
+router.get('/depsblob.js', function(req, res) {
+    depsBlob().bundle().pipe(res);
+});
+
+//router.use(require('serve-static')('./'));
+
+// Listen
+var app = express()
+var server = app.listen(15900)
+
+app.use(router)
+
+server.on('listening', function() {
+     console.log('Development server listening on: '+server.address().port)
+})

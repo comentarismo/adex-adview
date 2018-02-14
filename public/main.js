6bc5aad5743210e289d2a008479f6a722c469db9
Ivo paunov <ivo.paunov@gmail.com>
2018-02-14 16:36:27 +0200

    get adview adunits

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 var Wallet = require('ethereumjs-wallet')
 var util = require('ethereumjs-util')
 var Buffer = require('buffer').Buffer
+var queryString = require('query-string')
+require('whatwg-fetch')
 
 // @TODO: use storage.js
 var id
@@ -11,18 +13,42 @@ if (localStorage.priv && localStorage.priv.length === 64) {
 	localStorage.priv = id.getPrivateKey().toString('hex')
 }
 
-console.log('Address: '+id.getChecksumAddressString())
+console.log('Address: ' + id.getChecksumAddressString())
 
 window.onload = adexLoadedCallback
 
-function adexLoadedCallback()
-{
-	// @TODO: this should parse the URL and include a JSONP to load the view
-	adexViewCallback({ imgSrc: 'http://blog.strem.io/wp-content/uploads/2017/08/Download-Stremio-for-desktop-3.png', width: 300, height: 250, url: 'https://www.stremio.com' })
+
+function getAdData(slotId, width, height) {
+	fetch('http://127.0.0.1:9710/a-d-e-x-view?slotId=' + encodeURIComponent(slotId))
+		.then(function (res) {
+			return res.json()
+		})
+		.then(function (res) {
+			console.log('fetch res', res)
+
+			let adunitIndex = Math.floor(Math.random() * res.length) //TEMP
+			let adunit = res[adunitIndex]._meta
+			let url = (adunit.ad_url || '').toLowerCase()
+
+			if (!/^https?:\/\//i.test(url)) {
+				url = 'http://' + url
+			}
+
+			// @TODO: this should parse the URL and include a JSONP to load the view
+			// NOTE: we should tahe the width and height from the adunit but we keep it in the models as number and have to map it from adex-constants
+			adexViewCallback({ imgSrc: 'http://localhost:8080/ipfs/' + encodeURIComponent(adunit.img.ipfs), width: width, height: height, url: url })
+		})
+		.catch(function (err) {
+			console.log('fetch res', err)
+		})
 }
 
-function adexViewCallback(data)
-{
+function adexLoadedCallback() {
+	var query = queryString.parse(location.search)
+	getAdData(query.slotId, query.width, query.height, query.slotId)
+}
+
+function adexViewCallback(data) {
 	console.log('Load adunit with data', data)
 
 	window.adeximg.src = data.imgSrc
@@ -33,13 +59,11 @@ function adexViewCallback(data)
 	signAndSendEv({ type: 'loaded', time: Date.now(), data: data })
 }
 
-function adexClickCallback(data)
-{
+function adexClickCallback(data) {
 	signAndSendEv({ type: 'clicked', time: Date.now(), data: data })
 }
 
-function signAndSendEv(ev)
-{
+function signAndSendEv(ev) {
 	var blob = JSON.stringify(ev)
 	var message = util.toBuffer(blob)
 	var hash = util.hashPersonalMessage(message)
@@ -51,11 +75,11 @@ function signAndSendEv(ev)
 		msg: ev,
 		hash: util.bufferToHex(hash),
 		addr: id.getChecksumAddressString(),
-		sig: { 
-			mode: 1, 
+		sig: {
+			mode: 1,
 			v: sig.v,
 			r: util.bufferToHex(sig.r),
-			s: util.bufferToHex(sig.s) 
+			s: util.bufferToHex(sig.s)
 		},
 	}
 

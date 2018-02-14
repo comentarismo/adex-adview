8ec29c46f75350d02313545200aaea4e31b983f7
Ivo paunov <ivo.paunov@gmail.com>
2018-02-14 18:53:11 +0200

    get auth signature

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ var util = require('ethereumjs-util')
 var Buffer = require('buffer').Buffer
 var queryString = require('query-string')
 require('whatwg-fetch')
+var NODE_BASE_URL = 'http://127.0.0.1:9710'
 
 // @TODO: use storage.js
 var id
@@ -17,9 +18,33 @@ console.log('Address: ' + id.getChecksumAddressString())
 
 window.onload = adexLoadedCallback
 
+//TODO: set it to localStorage
+function getAuthSig() {
+	var authToken = (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)).toString()
+	var sigAndHash = signMsg(authToken)
+
+	var toSend = {
+		userid: id.getChecksumAddressString(),
+		signature: sigAndHash.sig.rpcSig,
+		mode: 1,
+		authToken: authToken
+	}
+
+	var headers = getHeaders('', '', '', [{
+		key: 'Content-Type',
+		value: 'application/json'
+	}])
+
+	fetch(NODE_BASE_URL + '/auth', {
+		method: 'POST',
+		headers: headers,
+		body: JSON.stringify(toSend)
+	})
+}
+
 
 function getAdData(slotId, width, height) {
-	fetch('http://127.0.0.1:9710/a-d-e-x-view?slotId=' + encodeURIComponent(slotId))
+	fetch(NODE_BASE_URL + '/a-d-e-x-view?slotId=' + encodeURIComponent(slotId))
 		.then(function (res) {
 			return res.json()
 		})
@@ -44,6 +69,7 @@ function getAdData(slotId, width, height) {
 }
 
 function adexLoadedCallback() {
+	getAuthSig()
 	var query = queryString.parse(location.search)
 	getAdData(query.slotId, query.width, query.height, query.slotId)
 }
@@ -63,25 +89,52 @@ function adexClickCallback(data) {
 	signAndSendEv({ type: 'clicked', time: Date.now(), data: data })
 }
 
-function signAndSendEv(ev) {
-	var blob = JSON.stringify(ev)
-	var message = util.toBuffer(blob)
+function signMsg(msg) {
+	var message = util.toBuffer(msg)
 	var hash = util.hashPersonalMessage(message)
 	var sig = util.ecsign(hash, id.getPrivateKey())
-
-	// @TODO: send, perhaps serialize the sig
-
-	var toSend = {
-		msg: ev,
+	console.log('signature', sig)
+	return {
 		hash: util.bufferToHex(hash),
-		addr: id.getChecksumAddressString(),
 		sig: {
 			mode: 1,
 			v: sig.v,
 			r: util.bufferToHex(sig.r),
-			s: util.bufferToHex(sig.s)
-		},
+			s: util.bufferToHex(sig.s),
+			rpcSig: util.toRpcSig(sig.v, sig.r, sig.s)
+		}
+	}
+}
+
+// JSON.stringify({ 'type': payload.type, 'uid': payload.uid, 'adunit': payload.adunit, 'address':  payload.address, ' signature' : payload.signature})]
+function signAndSendEv(ev) {
+	var blob = JSON.stringify(ev)
+	var sigAndHash = signMsg(blob)
+
+	// @TODO: send, perhaps serialize the sig
+
+	var toSend = {
+		msg: blob,
+		hash: sigAndHash.hash,
+		addr: id.getChecksumAddressString(),
+		sig: sigAndHash.sig
 	}
 
 	console.log(toSend)
+}
+
+function getHeaders(userAddr, authSig, authToken, otherHeaders) {
+	otherHeaders = otherHeaders || []
+	var hdrs = {
+		'X-User-Address': userAddr,
+		'X-User-Signature': authSig,
+		'X-Auth-Token': authToken
+	}
+
+	for (let index = 0; index < otherHeaders.length; index++) {
+		var hdr = otherHeaders[index]
+		hdrs[hdr.key] = hdr.value
+	}
+
+	return hdrs
 }
